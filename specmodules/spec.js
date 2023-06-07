81a7fa7a6908e7c701b8876d3cdeaa21c66b9d63
chihoonahn0319 <clgnseowkd@naver.com>
2023-06-07 16:20:23 +0900

    specReviewfinal

--- specmodules/spec.js
+++ specmodules/spec.js
@@ -1,1 +1,10 @@
-import {revier} from "./specReview - 치훈.js"
+import {addComment,editComment,deleteComment,saveComment,generateCommentId,getStarRating,init} from "./specReview - 치훈.js"
+
+addComment()
+editComment()
+deleteComment()
+saveComment()
+generateCommentId()
+getStarRating()
+init()
+
